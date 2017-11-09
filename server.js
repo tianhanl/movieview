e90b4f8af58809812d6ca7d5d7077e54f982067e
Tianhang Liu <tianhanl@usc.edu>
2017-11-08 18:55:37 -0800

    add basic server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(_dirname, 'dist')));
+
+app.get('/', (req, res) => {
+  res.type('.html');
+  // eslint-disable-next-line no-undef
+  res.sendFile(`${_dirname}/index.html`);
+});
+
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Page Not Found');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Server Started');
+});
+

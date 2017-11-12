0d98cdd92287ffed4475420c4482cf0bde551332
Tianhang Liu <tianhanl@usc.edu>
2017-11-11 23:42:11 -0800

    set detail view to responsive

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -6,7 +6,7 @@
         </div>
         <div class="detail-view-info">
             <h2 class="detail-view-info-title"><a :href="homepage">{{title}}</a></h2>
-            <p class="detail-view-info-date">{{date}}</p>
+            <p class="detail-view-info-date">{{formattedMovieDate}}</p>
             <p class="detail-view-info-genre">{{genres.join(', ')}}</p>
             <p class="detail-view-info-overview">{{overview}}</p>
             <p class="detail-view-info-score">Score: {{vote}}</p>
@@ -15,6 +15,7 @@
 </template>
 <script>
     import api from '../api';
+    import moment from 'moment';
     export default {
         data() {
           return {
@@ -33,6 +34,15 @@
       computed: {
         posterAddress() {
           return 'https://image.tmdb.org/t/p/w500/'+this.imageSrc;
+        },
+        formattedMovieDate() {
+          let preDate = moment(this.date);
+          if(preDate.isValid()) {
+            return preDate.format("MMM DD, YYYY");
+          } else {
+            return "N/A"
+          }
+
         }
       },
         created() {
@@ -67,47 +77,61 @@
         display: flex;
         justify-content: center;
         position: relative;
+        @media (max-width: 768px){
+            flex-direction: column;
+            align-items: center;
+        }
     }
     .detail-view-media {
-        flex: 1 0 400px;
+        flex: 1 1 400px;
         padding: 1em;
         max-width: 500px;
         text-align: center;
-        .detail-view-media-poster {
-            width: 400px;
-            height: auto;
+    }
+    .detail-view-media-poster {
+        width: 400px;
+        height: auto;
+        margin: auto;
+        @media (max-width: 767px){
+            width: 90%;
         }
-        .detail-view-media-tag {
-            margin: auto;
-            width: 400px;
-            font-size: 1.5em;
+    }
+    .detail-view-media-tag {
+        margin: auto;
+        width: 400px;
+        font-size: 1.5em;
+        @media (max-width:  768px){
+            width: 90%;
         }
     }
     .detail-view-info {
-        flex: 1 0 400px;
+        flex: 1 1 400px;
         max-width: 460px;
-        padding: 0 0.5em;
-        .detail-view-info-title {
-            font-size: 2.5em;
-            margin-bottom: 2rem;
-            a {
-                color: #4A4A4A;
-            }
+        padding: 0 1em;
+        @media (max-width:  768px){
+            width: 90%;
         }
-        .detail-view-info-date {
-            font-size: 1.5em;
-            margin: 0;
+    }
+    .detail-view-info-title {
+        font-size: 2.5em;
+        margin-bottom: 2rem;
+        a {
+            color: #4A4A4A;
         }
-        .detail-view-info-genre {
-            font-size: 1.5em;
-            margin: 0;
-        }
-        .detail-view-info-overview {
-            margin-top: 1em;
-        }
-        .detail-view-info-score {
-            font-size: 1.5em;
-        }
+    }
+    .detail-view-info-date {
+        font-size: 1.5em;
+        margin: 0;
+    }
+    .detail-view-info-genre {
+        font-size: 1.5em;
+        margin: 0;
+    }
+    .detail-view-info-overview {
+        margin-top: 1em;
+    }
+    .detail-view-info-score {
+        font-size: 1.5em;
     }
 
 </style>
